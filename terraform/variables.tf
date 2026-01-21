7d95a0237fcade570f9a395f14d0140e65a74aa6
cacti-lfs <lois.dutour@outlook.fr>
2026-01-21 15:24:32 +0100

    terraform: provider.tf created

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "api_token" {
+    type = string
+    sensitive = true
+    # Tu passeras la valeur via un fichier terraform.tfvars ou une variable d'environnement
+    # Format attendu par bpg : "USER@REALM!TOKENID=UUID"
+}
+
+variable "endpoint_proxmox" {
+    type = string
+    sensitive = false
+}
