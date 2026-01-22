a9812b8e9029f2ce7d0752ba53248930478b48b5
cacti-lfs <lois.dutour@outlook.fr>
2026-01-22 10:17:49 +0100

    terraform: main.tf finished & apply tested

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,22 @@
-variable "api_token" {
-    type = string
-    sensitive = true
-    # Tu passeras la valeur via un fichier terraform.tfvars ou une variable d'environnement
-    # Format attendu par bpg : "USER@REALM!TOKENID=UUID"
+variable "pm_api_token_id" {
+    type        = string
+    description = "ID du token API Proxmox"
+}
+
+variable "pm_api_token_secret" {
+    type        = string
+    sensitive   = true
+    description = "Le secret du token"
 }
 
 variable "endpoint_proxmox" {
     type = string
     sensitive = false
+    description = "URL de Proxmox"
 }
 
-variable "user_ssh" {
+variable "ssh_rsa_key" {
     type = string
-    sensitive = false
+    sensitive = true
+    description = "Clef publique SSH pour les VMS"
 }
